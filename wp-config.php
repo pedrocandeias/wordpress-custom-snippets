b07769947558855a65679c99ea814d5067e2df09
pedrocandeias <pedrocandeias@gmail.com>
2021-03-04 15:26:48 +0000

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,32 @@
+// AUTOMATICALLY empty trash every 30 days
+define( 'EMPTY_TRASH_DAYS', 30 ); 
+
+// Define a autosave interval in seconds
+define( 'AUTOSAVE_INTERVAL', 180 ); 
+
+// Limit number of post revisions
+define( 'WP_POST_REVISIONS', 5 );
+
+// Disable post revisions
+define( 'WP_POST_REVISIONS', false );
+
+// Change WP Site URL 
+define( 'WP_SITEURL', 'http://example.com/wordpress' );
+
+// Change Home
+define( 'WP_HOME', 'http://example.com' );
+
+// Disable file editing
+define( 'DISALLOW_FILE_EDIT', true );
+
+// Disable auto updates
+define( 'AUTOMATIC_UPDATER_DISABLED', true );
+
+// Define WP memory limit
+define( 'WP_MEMORY_LIMIT', '96M' );
+
+// Clean up old images
+define( 'IMAGE_EDIT_OVERWRITE', true );
+
+// Repair database
+ define( 'WP_ALLOW_REPAIR', true );
