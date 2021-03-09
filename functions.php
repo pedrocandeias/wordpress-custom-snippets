4e7c7171134499a076a257ab53b68b349cd64e08
pedrocandeias <pedrocandeias@gmail.com>
2021-03-09 15:46:04 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -69,17 +69,76 @@ function smartwp_custom_logo_wp_dashboard() {
 add_action('wp_before_admin_bar_render', 'smartwp_custom_logo_wp_dashboard');
 
 //Enable SVG upload
-function smartwp_enable_svg_upload( $mimes ) {
-  //Only allow SVG upload by admins
-  if ( !current_user_can( 'administrator' ) ) {
-    return $mimes;
-  }
-  $mimes['svg']  = 'image/svg+xml';
-  $mimes['svgz'] = 'image/svg+xml';
-  
-  return $mimes;
+
+function svgs_upload_mimes( $mimes = array() ) {
+
+	global $svgs_options;
+
+	if ( empty( $svgs_options['restrict'] ) || current_user_can( 'administrator' ) ) {
+
+		// allow SVG file upload
+		$mimes['svg'] = 'image/svg+xml';
+		$mimes['svgz'] = 'image/svg+xml';
+
+		return $mimes;
+
+	} else {
+
+		return $mimes;
+
+	}
+
 }
-add_filter('upload_mimes', 'smartwp_enable_svg_upload');
+add_filter( 'upload_mimes', 'svgs_upload_mimes', 99 );
+
+/**
+ * Check Mime Types
+ */
+function svgs_upload_check( $checked, $file, $filename, $mimes ) {
+
+	if ( ! $checked['type'] ) {
+
+		$check_filetype		= wp_check_filetype( $filename, $mimes );
+		$ext				= $check_filetype['ext'];
+		$type				= $check_filetype['type'];
+		$proper_filename	= $filename;
+
+		if ( $type && 0 === strpos( $type, 'image/' ) && $ext !== 'svg' ) {
+			$ext = $type = false;
+		}
+
+		$checked = compact( 'ext','type','proper_filename' );
+	}
+
+	return $checked;
+
+}
+add_filter( 'wp_check_filetype_and_ext', 'svgs_upload_check', 10, 4 );
+
+/**
+ * Mime Check fix for WP 4.7.1 / 4.7.2
+ *
+ * Fixes uploads for these 2 version of WordPress.
+ * Issue was fixed in 4.7.3 core.
+ */
+function svgs_allow_svg_upload( $data, $file, $filename, $mimes ) {
+
+	global $wp_version;
+	if ( $wp_version !== '4.7.1' || $wp_version !== '4.7.2' ) {
+		return $data;
+	}
+
+	$filetype = wp_check_filetype( $filename, $mimes );
+
+	return [
+		'ext'				=> $filetype['ext'],
+		'type'				=> $filetype['type'],
+		'proper_filename'	=> $data['proper_filename']
+	];
+
+}
+add_filter( 'wp_check_filetype_and_ext', 'svgs_allow_svg_upload', 10, 4 );
+
 
 //Disable XML-RPC
 add_filter('xmlrpc_enabled', '__return_false');
